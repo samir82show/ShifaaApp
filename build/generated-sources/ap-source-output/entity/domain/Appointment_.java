4071195d6a3129eb63f782216876a0340b723d3f
samir82show <samir82show@hotmail.com>
2018-11-16 23:40:17 +0300

    update - 16/11/2018 23:40:17.36

--- build/generated-sources/ap-source-output/entity/domain/Appointment_.java
+++ build/generated-sources/ap-source-output/entity/domain/Appointment_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-16T19:23:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-16T23:37:54")
 @StaticMetamodel(Appointment.class)
 public class Appointment_ { 
 
--- build/generated-sources/ap-source-output/entity/domain/Insurance_.java
+++ build/generated-sources/ap-source-output/entity/domain/Insurance_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-16T19:23:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-16T23:37:54")
 @StaticMetamodel(Insurance.class)
 public class Insurance_ { 
 
