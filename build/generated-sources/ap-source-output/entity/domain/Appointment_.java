755e315afb1b41b39a115a9b487dd7a8bcf0e359
samir82show <samir82show@hotmail.com>
2018-11-16 19:28:32 +0300

    update - 16/11/2018 19:28:32.44

--- build/generated-sources/ap-source-output/entity/domain/Appointment_.java
+++ build/generated-sources/ap-source-output/entity/domain/Appointment_.java
@@ -10,23 +10,21 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-15T11:46:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-16T19:23:42")
 @StaticMetamodel(Appointment.class)
 public class Appointment_ { 
 
     public static volatile SingularAttribute<Appointment, Gender> gender;
-    public static volatile SingularAttribute<Appointment, String> rating;
-    public static volatile SingularAttribute<Appointment, String> description;
-    public static volatile SingularAttribute<Appointment, String> ratingComment;
-    public static volatile SingularAttribute<Appointment, Gender> doctorGender;
-    public static volatile ListAttribute<Appointment, DaysOfWeek> daysOfWeeks;
-    public static volatile SingularAttribute<Appointment, Shift> morningOrEvening;
     public static volatile SingularAttribute<Appointment, String> phone;
     public static volatile SingularAttribute<Appointment, String> inArabic;
     public static volatile SingularAttribute<Appointment, Date> DOB;
     public static volatile SingularAttribute<Appointment, String> name;
+    public static volatile SingularAttribute<Appointment, String> description;
+    public static volatile SingularAttribute<Appointment, Gender> doctorGender;
     public static volatile SingularAttribute<Appointment, Long> id;
     public static volatile SingularAttribute<Appointment, Hospital> hospital;
+    public static volatile ListAttribute<Appointment, DaysOfWeek> daysOfWeeks;
     public static volatile SingularAttribute<Appointment, String> email;
+    public static volatile SingularAttribute<Appointment, Shift> morningOrEvening;
 
 }
--- build/generated-sources/ap-source-output/entity/domain/HospitalImage_.java
+++ build/generated-sources/ap-source-output/entity/domain/HospitalImage_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-15T11:46:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-16T19:23:42")
 @StaticMetamodel(HospitalImage.class)
 public class HospitalImage_ { 
 
