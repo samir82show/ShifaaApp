3b65a27a9f1d20dc7a895719b0a4946100e0681a
samir82show <samir.ahmedmustafa@gmail.com>
2019-02-15 20:52:58 +0300

    update - Fri 02/15/2019 20:52:58.82

--- build/generated-sources/ap-source-output/entity/domain/Clinic_.java
+++ build/generated-sources/ap-source-output/entity/domain/Clinic_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-02-15T16:32:26")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-02-15T18:57:48")
 @StaticMetamodel(Clinic.class)
 public class Clinic_ { 
 
