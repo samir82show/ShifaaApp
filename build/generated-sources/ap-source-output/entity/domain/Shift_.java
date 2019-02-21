f240c93a6a45d7eb88cd0113d6b11de1c79b60ea
Samir Ahmed <samir.ahmedmustafa@gmail.com>
2019-02-21 16:13:54 +0300

    fixing upload dir - 21/02/2019 16:13:53.96

--- build/generated-sources/ap-source-output/entity/domain/Shift_.java
+++ build/generated-sources/ap-source-output/entity/domain/Shift_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-02-15T18:57:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-02-21T16:08:07")
 @StaticMetamodel(Shift.class)
 public class Shift_ { 
 
