4b49addecfcbc6b7824015714dfa4a95e7044aba
Rania29 <37171434+Rania29@users.noreply.github.com>
2019-04-11 00:15:25 +0300

    Console style

--- build/generated-sources/ap-source-output/entity/domain/Insurance_.java
+++ build/generated-sources/ap-source-output/entity/domain/Insurance_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-10T23:51:36")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-11T00:10:57")
 @StaticMetamodel(Insurance.class)
 public class Insurance_ { 
 
